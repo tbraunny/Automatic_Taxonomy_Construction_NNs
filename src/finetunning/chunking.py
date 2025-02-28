23606fc001de34cadaab04a68a3070ec2ce50234
Chase Carthen <chasec2331@gmail.com>
2025-02-28 15:30:18 -0800

    fixing issues

--- src/finetunning/chunking.py
+++ src/finetunning/chunking.py
@@ -6,7 +6,7 @@ import glob
 import os
 from docling.document_converter import DocumentConverter
 
-def split_text(text,chunk=2048):
+def split_text(text,chunk_size=2048):
     chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
     return chunks
 
@@ -14,11 +14,14 @@ def parse_pdf(path):
     converter = DocumentConverter()
     result = converter.convert(path)
     output = result.document.export_to_markdown()
+    handle = open(f'converted/os.path.splitext(os.path.basename(path))[0]','w')
+    handle.write(output)
+    handle.close()
     return output
 
 if __name__ == '__main__':
 
-    data = glob.glob('data/*.txt') + glob.glob('data/*.md') + glob.glob('data/*.pdf')
+    data = glob.glob("data/*.owl") + glob.glob("data/*.xml") + glob.glob('data/*.txt') + glob.glob('data/*.md') + glob.glob('data/*.pdf')
     chunked_text = []
     for path in data:
         if path.endswith('.pdf'):
@@ -34,7 +37,7 @@ if __name__ == '__main__':
         writer = csv.writer(csv_file)
         writer.writerow(["text"])  # CSV header
         
-        for chunk in chunks:
+        for chunk in chunked_text:
             writer.writerow([chunk])
 
-    print(f"Created '{output_csv}' with {len(chunks)} rows.")
+    print(f"Created '{output_csv}' with {len(chunked_text)} rows.")
