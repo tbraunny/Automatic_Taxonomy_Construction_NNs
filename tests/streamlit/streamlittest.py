4523b4476a012de67acd65419cb68949c6f8f697
Lac-Lukas <lukaslac@yahoo.com>
2025-03-06 16:10:12 -0800

    graphRAG branch, graphRAG -> tests.ipynb (for chase)

--- tests/streamlit/streamlittest.py
+++ tests/streamlit/streamlittest.py
@@ -7,7 +7,7 @@ st.markdown("[Thomas Braun](http://localhost:8866/)")
 voila_url = "http://127.0.0.1:8866/"
 
 # Embed the notebook
-iframe_code = f'<iframe src="{voila_url}" width="100%" height="800"></iframe>'
+iframe_code = f'<iframe src="{voila_url}" width="500" height="800"></iframe>'
 st.markdown(iframe_code, unsafe_allow_html=True)
 
 
