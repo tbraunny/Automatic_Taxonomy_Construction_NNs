0025c9370f317f3a247734f4cedbfc81b234414f
white-richard <rich.white285@yahoo.com>
2025-04-17 17:51:17 -0700

    deleted a bunch of garbo

--- tests/llama/dependencycheck.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-if torch.cuda.is_available():
-    print("CUDA is available!")
-else:
-    print("CUDA is not available. Using CPU.")
-
-import langchain
-print(langchain.__version__)
