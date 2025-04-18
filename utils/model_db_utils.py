5c62e86c8fda5dcf292c746931341719206bdb4a
white-richard <rich.white285@yahoo.com>
2025-04-18 15:06:11 -0700

    only added the files that I debugged, changes only to db utils, code extractor, and main

--- utils/model_db_utils.py
+++ utils/model_db_utils.py
@@ -182,7 +182,7 @@ class DBUtils:
         except Exception as e:
             logging.exception(f"Failed to insert parameter {name} into layer {layer_id} in the database: {e}")
 
-    def insert_papers(self , paper_name: str , contents: str) -> int:
+    def insert_papers(self , ann_path: str) -> int:
         """
         Insert paper for model into the database
 
@@ -192,17 +192,24 @@ class DBUtils:
         """
         paper_id = -1
         try:
-            query = text("""INSERT INTO paper (paper_name , contents)
-                         VALUES (:paper_name , :contents)
-                         RETURNING paper_id""")
-            result = self.session.execute(query , {
-                "paper_name": paper_name,
-                "contents": contents
-            })
-            paper_id = result.scalar()
-            self.session.commit()
+            pdf_files: list = glob.glob(f"{ann_path}/**/*doc*.pdf" , recursive=True)
+
+            for pdf in pdf_files:
+                contents: dict = {}
+                with open(pdf , 'r') as f:
+                    contents = json.load(f)
+
+                query = text("""INSERT INTO paper (paper_name , contents)
+                            VALUES (:paper_name , :contents)
+                            RETURNING paper_id""")
+                result = self.session.execute(query , {
+                    "paper_name": ann_path,
+                    "contents": contents
+                })
+                paper_id = result.scalar()
+                self.session.commit()
         except Exception as e:
-            logging.exception(f"Failed to insert paper {paper_name} into database: {e}")
+            logging.exception(f"Failed to insert paper {ann_path} into database: {e}")
 
         return paper_id
     
@@ -270,26 +277,27 @@ class DBUtils:
         for file in json_files:
             network_data: dict = {}
 
+            print(file)
+
             with open(file , 'r') as f:
-                network_data: dict = json.load()
+                network_data: dict = json.load(f)
 
             nodes = network_data.get("graph" , {}).get("node" , {})
             model_type = None
-            model_id: int = self.insert_model(ann_name , model_type , network_data)
+            model_id: int = self._insert_model(ann_path , model_type , network_data)
 
             for layer in nodes:
                 layer_name = layer.get('name')
                 layer_type = layer.get('op_type')
                 layer_attr = layer.get('attributes')
 
-                layer_id = self.insert_layer(model_id , layer_name , layer_type , layer_attr)
+                layer_id = self._insert_layer(model_id , layer_name , layer_type , layer_attr)
                 shape = layer.get('kernel')
 
-                if layer_id and layer_attr:
-                    for attr in layer_attr:
-                        param_name: str = attr.get('name')
-                        shape: list = attr.get('ints')
-                        self.insert_parameter(layer_id , param_name , shape)
+                # if layer_id and layer_attr:
+                #     for param_name in layer_attr:
+                #         shape: list = param_name.get('ints')
+                #         self._insert_parameter(layer_id , param_name , shape)
 
     
 if __name__ == '__main__':
